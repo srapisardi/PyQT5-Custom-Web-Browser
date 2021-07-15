9b72d3d86300681ce726d2c58801cb10d435493e
srapisardi <50967041+srapisardi@users.noreply.github.com>
2021-07-15 00:20:54 -0400

    Add files via upload
    Added reload, back, and forward buttons as well as URL entry

--- Web Browser/browser.py
+++ Web Browser/browser.py
@@ -17,10 +17,36 @@ class MainWindow(QMainWindow):
         nav.setIconSize(QSize(24,24))
         self.addToolBar(nav)
 
-        home_btn = QAction(QIcon('home-icon.png'), 'Click to open Home page', self)
+        home_btn = QAction(QIcon('home-icon.png'), 'Click to go home', self)
         home_btn.triggered.connect(self.browser.back)
         nav.addAction(home_btn)
 
+        back_btn = QAction(QIcon('back-icon.png'), 'Click to go back', self)
+        back_btn.triggered.connect(self.browser.back)
+        nav.addAction(back_btn)
+
+        forward_btn = QAction(QIcon('forward-icon.png'), 'Click to go forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        nav.addAction(forward_btn)
+
+        reload_btn = QAction(QIcon('reload-icon.png'), 'Click to reload page', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        nav.addAction(reload_btn)
+
+        self.url_entry = QLineEdit()
+        self.url_entry.returnPressed.connect(self.navigate_to_url)
+        nav.addWidget(self.url_entry)
+        self.browser.urlChanged.connect(self.update_url)
+
+    def home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+
+    def navigate_to_url(self):
+        url = self.url_entry.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, entered_url):
+        self.url_entry.setText(entered_url.toString())
 
 
 
